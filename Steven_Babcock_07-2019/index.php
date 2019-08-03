290176eff3f3a8dd8ae07f0f311049070830cf3e
Steven Babcock <sbabcock20@gmail.com>
2019-08-02 21:54:04 -0500

    added bootstrap for better control over responsive design, updated heading 2 columns to repsond better to tablets and mobile devices

--- Steven_Babcock_07-2019/index.php
+++ Steven_Babcock_07-2019/index.php
@@ -4,7 +4,15 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link 
+      rel="stylesheet" 
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+      crossorigin="anonymous"/>
    <link rel="stylesheet" href="style.css">
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
    <script src="puzzle.js"></script>
    
@@ -19,51 +27,56 @@
    JS puzzle Done
    Respnsiveness for mobile.-- Not much to change. looks and preforms well across phones, tablets, and desktops.  -->
    <!-- header -->
-   <header class="header"><img class="logo" src="/mwi-benchmark\img\mwi-logo-horizontal.png" alt="logo" /></header>
-   <h1 class="mainH1">Heading One</h1>
-   <div class="infoDiv">
-   <div class="columns">
-      <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
-      <h2>Heading 2</h2>
-      <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
-      <button>Read More</button>
+   <header class="header"><img class="logo" src="../img/mwi-logo-horizontal.png" alt="logo" /></header>
+   <div class="container">
+      <h1 class="mainH1">Heading One</h1>
+   <div class="row">
+      <div class="col-12 col-md-6 col-lg-4">
+         <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
+         <h2>Heading 2</h2>
+         <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
+         <button class="button">Read More</button>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+         <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
+         <h2>Heading 2</h2>
+         <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
+         <button class="button">Read More</button>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+         <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
+         <h2>Heading 2</h2>
+         <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
+         <button class="button">Read More</button>
+      </div>
    </div>
-   <div class="columns">
-      <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
-      <h2>Heading 2</h2>
-      <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
-      <button>Read More</button>
    </div>
-   <div class="columns">
-      <div class="imgBox">this is a dummy box where i would put an img as a banner that matches the type of content for this info table </div>
-      <h2>Heading 2</h2>
-      <p>This is a paragraph text that is meant to flex and wrap to a new line based on the number of characters in this container. Please make sure this is looking pretty based on the amount of characters this is taking up.</p>
-      <button>Read More</button>
-   </div>
-</div>
 <div class="contactInfo">
    <!-- CONTACT FORM -->
    <main>
    <p>Contact</p>
-   <form class="contact-form" action="contactform.php" method="post">
+   <form class="contactForm" action="contactform.php" method="post">
       <p class="contactTitles">Email</p>
-      <input type="text" name="email" placeholder="Your Email" required>
+      <input class="inputField" type="text" name="email" placeholder="Your Email" required>
       <p class="contactTitles">Subject</p>
-      <input type="text" name="subject" placeholder="Subject" required>
+      <input class="inputField" type="text" name="subject" placeholder="Subject" required>
       <p class="contactTitles">Message</p>
-      <textarea name="message" placeholder="Type message here" required></textarea>
+      <textarea class="inputField" name="message" placeholder="Type message here" required></textarea>
       <button type="submit" name="submit">Submit</button>
    </form>
    </main>
 </div>
 <!-- Javascript Puzzle -->
-<div class="javaScriptPuzzle">
-   <h1>Heading One</h1>
+<div class="container javaScriptPuzzle">
+   <div class="row">
+      <div class="col-12">
+         <h1>Heading One</h1>
    <p>Remove the duplicates of 2 Javascript objects and output the list of distinct names in and unordered list when <a id="myLink" href="">this link is pressed</a>, if the operation has been completed already notify the user that this has already been done.</p>
    <ul>Returned Results</ul>
    <ul id="list" class="list">   
    </ul>
-
+      </div>
+   </div>
 </div>
 </body>
 </html>
--- Steven_Babcock_07-2019/puzzle.js
+++ Steven_Babcock_07-2019/puzzle.js
@@ -19,64 +19,67 @@
 // Tyler Viles
 
 //Event listener to listen for the content to be loaded on the page
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function() {
+  //boolean to be used for page alert, once link is clicked, the status will change and allow the page alert to be executed
+  let didShowResults = false;
 
-   //boolean to be used for page alert, once link is clicked, the status will change and allow the page alert to be executed
-   let didShowResults = false;
+  // connect to the link
+  const myLinkElm = document.getElementById("myLink");
 
-   // connect to the link
-   const myLinkElm = document.getElementById("myLink");
-   
-   //
-   function showResults(event) {
-         if (event && event.preventDefault) {
-            event.preventDefault();
-         }
-         if (didShowResults) {
-            alert('Results have already been given at the bottom of the page.');
-         return;
-         }
-   // Names colleted from gitHub, sorted into arrays.
-   let names1 = ["Matt Johnson", "Bart Paden", "Ryan Doss", "Michael Spangler"];
-   let names2 = ["Matt Johnson", "Bart Paden", "Jordan Heigle", "Tyler Viles"];
-   // merge arrays together and return a new array to be manipulated
-   let names3 = names1.concat(names2);   
-   let results = [];
-   let len = names3.length;
-   
-   // for loop to iterate through names3 array and return unique values to the results array
-   for (let i = 0; i < len; i++) {
-         const item = names3[i];
-   if (!results.includes(item)) {
-         results.push(item);
-   }
-   }
-   //check to see if results are all unique values
-   console.log(results);
+  //
+  function showResults(event) {
+    if (event && event.preventDefault) {
+      event.preventDefault();
+    }
+    if (didShowResults) {
+      alert("Results have already been given at the bottom of the page.");
+      return;
+    }
+    // Names colleted from gitHub, sorted into arrays.
+    let names1 = [
+      "Matt Johnson",
+      "Bart Paden",
+      "Ryan Doss",
+      "Michael Spangler"
+    ];
+    let names2 = ["Matt Johnson", "Bart Paden", "Jordan Heigle", "Tyler Viles"];
+    // merge arrays together and return a new array to be manipulated
+    let names3 = names1.concat(names2);
+    let results = [];
+    let len = names3.length;
 
-   // FILTER RESULTS ARRAY INTO AN UNORDERED LIST
+    // for loop to iterate through names3 array and return unique values to the results array
+    for (let i = 0; i < len; i++) {
+      const item = names3[i];
+      if (!results.includes(item)) {
+        results.push(item);
+      }
+    }
+    //check to see if results are all unique values
+    console.log(results);
 
-   //connect to HTML ID tag for list
-   const unorderedList = document.getElementById("list");
-   //helper function to create a new LI, and append results for an Unordered List
-         const addToUnorderedList = (name) => {
-         //creats a new LI in the HTML doc
-            const li = document.createElement('li');
-            li.innerText = name;
-            li.setAttribute('id', name);
-            unorderedList.appendChild(li);
-   }
-   //forEach loop to grab each value of the results array and run the add to UL function above
-   results.forEach((name, index, arr) => {
+    // FILTER RESULTS ARRAY INTO AN UNORDERED LIST
+
+    //connect to HTML ID tag for list
+    const unorderedList = document.getElementById("list");
+    //helper function to create a new LI, and append results for an Unordered List
+    const addToUnorderedList = name => {
+      //creats a new LI in the HTML doc
+      const li = document.createElement("li");
+      li.innerText = name;
+      li.setAttribute("id", name);
+      unorderedList.appendChild(li);
+    };
+    //forEach loop to grab each value of the results array and run the add to UL function above
+    results.forEach((name, index, arr) => {
       addToUnorderedList(name);
-   //log to make sure the names are being added to the list
-   console.log(`Added "${name}" to the list!`);
-   });
-   
-   //changes to reflect the link being clicked and allow access to the page alert for multiple clicks
-   didShowResults = true;
-   }
-   //page alert to inform the user that the results have been sorted into an unordered list
-   myLinkElm.addEventListener('click', showResults);
-   
-   });
+      //log to make sure the names are being added to the list
+      console.log(`Added "${name}" to the list!`);
+    });
+
+    //changes to reflect the link being clicked and allow access to the page alert for multiple clicks
+    didShowResults = true;
+  }
+  //page alert to inform the user that the results have been sorted into an unordered list
+  myLinkElm.addEventListener("click", showResults);
+});
